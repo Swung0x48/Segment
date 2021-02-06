5e6a6be655223f0ba9caa92f6158e4e4ffb3d45d
Swung0x48 <swung0x48@outlook.com>
2021-02-07 01:02:33 +0800

    Bump version number

--- Segment/Segment.h
+++ Segment/Segment.h
@@ -3,7 +3,7 @@
 #include <sstream>
 constexpr int SEG_MAJOR_VER = 1;
 constexpr int SEG_MINOR_VER = 0;
-constexpr int SEG_PATCH_VER = 23;
+constexpr int SEG_PATCH_VER = 24;
 
 extern "C" {
 	__declspec(dllexport) IMod* BMLEntry(IBML* bml);
